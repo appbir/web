0296807078b84032ac72955323298849207f7e7b
appbir <appbir@163.com>
2019-04-09 11:40:34 +0800

    ready react develop envelopment

---
+++ src/module/demo/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './index.less';
+class ReactBoilerplate extends React.Component {
+    render() {
+      return (<div className="welcome">
+                <h1>Hello, Wellcome Appbir React ReactBoilerplate</h1>
+            </div>)
+    }
+  }
+
+export default ReactBoilerplate;
---
+++ src/module/learn/deepcode/library/react/lifecycle/lifecycle.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './lifecycle.less';
+class ReactLifecycle extends React.Component {
+    render() {
+      return (<div className="lifecycle">
+                <h1>react生命周期</h1>
+            </div>)
+    }
+  }
+
+export default ReactLifecycle;
